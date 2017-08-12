56f3d0e97a0f21961cc290e1a90ccf37646527b5
nusrat jahan <xinia214@gmail.com>
2017-08-13 02:27:53 +0600

    the ExceptionHandling Conundrum

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -19,21 +19,31 @@ class CreateThreadTest extends TestCase
     }
 
     /** @test */
-    function an_authenticated_user_can_create_forum_threads(){
 
-    //    we have authenticated user
-
-        $this->actingAs(create('App\User'));
-
-        // user can create threads
-
-        $thread = make('App\Thread');
-
-        $this->post('/threads', $thread->toArray());
-        $this->get($thread->path())
-            ->assertSee($thread->title)
-            ->assertSee($thread->body);
-
+    function guests_can_not_see_create_thread_page()
+    {
+        $this->withExceptionHandling()
+            ->get('threads/create')
+            ->assertRedirect('/login');
 
     }
+//
+//    /** @test */
+//    function an_authenticated_user_can_create_forum_threads(){
+//
+//    //    we have authenticated user
+//
+//        $this->actingAs(create('App\User'));
+//
+//        // user can create threads
+//
+//        $thread = make('App\Thread');
+//
+//        $this->post('/threads', $thread->toArray());
+//        $this->get($thread->path())
+//            ->assertSee($thread->title)
+//            ->assertSee($thread->body);
+//
+//
+//    }
 }
